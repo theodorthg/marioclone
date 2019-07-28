78b43da23cbe6f46f6c0f1f38de65225c37c946f
theodorthg <theodorthg@live.de>
2019-07-28 18:58:32 +0200

    Tutorial 2 / 12
    Camera Movement (smoothly)

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
+    public Transform target;
+    public Transform leftBounds;
+    public Transform rightBounds;
+
+	public float smoothDampTime = 0.15f;
+	private float smoothDampVelocity = 0.0f; //Vector3.zero; BeBi
+
+    private float camWidth, camHeight, levelMinX, levelMaxX;
+
 	// Use this for initialization
 	void Start () {
+		camHeight = Camera.main.orthographicSize * 2;
+		camWidth = camHeight * Camera.main.aspect;
+
+		float leftBoundsWidth = leftBounds.GetComponentInChildren<SpriteRenderer> ().bounds.size.x;
+		float rightBoundsWidth = rightBounds.GetComponentInChildren<SpriteRenderer> ().bounds.size.x;
+
+		levelMinX = leftBounds.position.x + leftBoundsWidth + (camWidth / 2);
+		levelMaxX = rightBounds.position.x - rightBoundsWidth - (camWidth / 2);
+
+
 
 	}
 
 	// Update is called once per frame
 	void Update () {
-		
+		if (target) {
+			float targetX = Mathf.Max (levelMinX, Mathf.Min (levelMaxX, target.position.x));
+			float x = Mathf.SmoothDamp (transform.position.x, targetX, ref smoothDampVelocity, smoothDampTime);
+
+			transform.position = new Vector3 (x, transform.position.y, transform.position.z);
+		}
+
 	}
 }
