6dab9bc4c2da130140c6243b071f2b0b8197e275
theodorthg <theodorthg@live.de>
2019-07-29 06:55:07 +0200

    Tutorial 3 / 12
    Some fixes (Background, some experiments with Camera-Object Backgrround and ClearFlags done)
    Added comments in german for Player- and CameraFollow-Scripts

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,36 +8,58 @@ public class CameraFollow : MonoBehaviour {
     public Transform leftBounds;
     public Transform rightBounds;
 
+	// Führt die Kamera nicht ruckartig zur neuen Position, sondern innerhalb einer angegebenen Zeit.
 	public float smoothDampTime = 0.15f;
 	private Vector3 smoothDampVelocity = Vector3.zero; // BeBi
 
     private float camWidth, camHeight, levelMinX, levelMaxX;
-
-	Camera cam;
 
 	// Use this for initialization
 	void Start () {
 		camHeight = Camera.main.orthographicSize * 2;
 		camWidth = camHeight * Camera.main.aspect;
 
-		float leftBoundsWidth = leftBounds.GetComponentInChildren<SpriteRenderer> ().bounds.size.x;
-		float rightBoundsWidth = rightBounds.GetComponentInChildren<SpriteRenderer> ().bounds.size.x;
+		/* Sprites sind für Mario 32 Pixel breit. Das könnte man fest setzen. 
+		 * Für einen allgemeineren Ansatz tun wir dies nicht.
+		 * 
+		 * leftBounds und rightBounds werden und müssen auch unbedingt an die Kamera gebunden werden,
+		 * in dem sie aus der Projekt-Hirarchie auf die entsprechenden Felder der Main-Kamera im Inspektor 
+		 * gezogen werden. Anfangs sind sie mit Bounds und Bounds(1) benannt. Sie müssen 
+		 * (passiert später im Tutorial 3) noch umbenannt werden.
+		 * 
+		 * Aber für andere Breiten muss herausgefunden werden, wie breit diese sind. 
+		 * Die x-Position (und y-Position) ist immer in der Mitte des Spielers. 
+		 * Daher muss durch 2 geteilt werden.
+		 */
+		float leftBoundsWidth = leftBounds.GetComponentInChildren<SpriteRenderer> ().bounds.size.x / 2;
+		float rightBoundsWidth = rightBounds.GetComponentInChildren<SpriteRenderer> ().bounds.size.x / 2;
 
+		// Die Kamera soll sich so auf den Spieler fokussieren, dass dieser immer rechts und links gleich
+		// viel Platz hat, sie also in der "Mitte des Levels" steht und dem Spieler sanft folgt.
 		levelMinX = leftBounds.position.x + leftBoundsWidth + (camWidth / 2);
 		levelMaxX = rightBounds.position.x - rightBoundsWidth - (camWidth / 2);
-		cam = GetComponent<Camera> ();
+
 	}
 
 	// Update is called once per frame
 	void Update () {
-		Camera cam = Camera.current;
+
+		// Wenn die Instance (also das Exemplar) von target existiert, also erzeugt wurde.
 		if (target) {
+			/* Hier wird sicher gestellt, dass die Grenzen die Linke und rechte Kante der Kamera sind
+			 * Es wird das Maximum berechnet aus der linken Grenze und dem 
+			 * Ergebnis aus dem Minimum aus rechter Grenze und aktueller Position.
+			 * Ist die aktuelle Position über der rechten Grenze, wird die Kamera "smooth" nachgezogen.
+			 * Das passiert mit SmoothDamp.Wirkt wie ein Gimbal.
+			 * Ebenso, wenn der Spieler die linke Grenze der Kamera-Box überschritten hat.
+			 */
+
 			float targetX = Mathf.Max (levelMinX, Mathf.Min (levelMaxX, target.position.x));
 			float x = Mathf.SmoothDamp (transform.position.x, targetX, ref smoothDampVelocity.x, smoothDampTime);
 
+			// Hier wird die neue Position der Kamera tatsächlich angewandt
 			transform.position = new Vector3(x, transform.position.y, transform.position.z);
 
-			// cam.backgroundColor = new Color (0, 0, 1,0.7f);
 		}
 
 	}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -43,8 +43,11 @@ public class Player : MonoBehaviour {
 
 	void CheckPlayerInput () {
 
+		// Hier können die Eingaben mittels GetKey abgefragt werden.
 		bool input_left = Input.GetKey (KeyCode.LeftArrow);
 		bool input_right = Input.GetKey (KeyCode.RightArrow);
+
+		// Springen schon dann, wenn die Taste noch nicht losgelassen wurde.
 		bool jump_key = Input.GetKeyDown (KeyCode.Space);
 
 		/*
@@ -57,9 +60,20 @@ public class Player : MonoBehaviour {
 		walk_right = (input_right || input_right_joy) && !(input_left || input_left_joy);
 		jump = input_jump_joy || jump_key;
 		*/
+
+		/*
+		 * OR-Operator wird benutzt, um zu sehen, ob entweder nach rechts ODER links gedrückt wurde.
+		 * Funktioniert im Moment (Tutorial 3 / 12) nur mit den Cursor-Tasten. Im Emulator Citra kann man
+		 * das passend belegen (auf die Buttons oder das Steuerkreuz).
+		 * Später werden hier auch die Buttons und das Steuerkreuz vom 3DS abgefragt.
+		 */ 
+	
 		walk = input_left || input_right;
+		// Wenn nach links gedrückt wurde UND NICHT nach rechts 
 		walk_left = (input_left) && !(input_right);
+		// Wenn nach rechts gedrückt wurde UND NICHT nach links
 		walk_right = (input_right) && !(input_left);
+		// Wenn jump gedrückt wurde (Space oder später Y)
 		jump = jump_key;
 
 	}
